74f68ab113407cc5afc3ba6ba00a3912140732e3
pedroribeiro3 <pedro.ribeiro3@estudante.ifms.edu.br>
2023-09-14 08:30:15 -0400

    Atualizando o projeto pt.2 14.09

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -9,7 +9,9 @@ type FilmeType = {
 }
 
 export default function Main() {
-    //Hooks são funções do React que ajudam a gente a fazer tarefas específicas
+    //let textodigitado = 'Barbie'
+    //Hooks são funções do React que ajudam a gente a fazer tarefas
+    //específicas
     const [texto,setTexto]=useState("")
 
     const filmes:FilmeType[] = [
@@ -23,20 +25,23 @@ export default function Main() {
             id: 2,
             titulo:'Barbie: Fairytopia',
             sinopse:'Logo após o arco-íris, em um mundo chamado Fairytopia, vive Elina, uma bela fada das flores que sonha em ter asas. Um dia, Elina percebe que sua casa de flor está doente e seus amigos não podem mais voar. Cheia de coragem, ela parte em uma fantástica jornada para encontrar Azura, a guardiã da Cidade das Fadas, já que ela não é afetada pelo feitiço que causou todos os problemas.',
-            imagem:'public/fairytopia.jpg',
+            imagem:'/fairytopia.jpg',
         },
         {   
             id: 3,
             titulo:'Barbie: Segredo das Fadas',
             sinopse: "Mundos mágicos, fadas e cavalos voadores são alguns dos elementos encontrados neste filme da Barbie. Ela precisa viajar a um mundo encantado para salvar Ken. Nesta aventura, serão revelados segredos e algumas amizades serão testadas.",
-            imagem:'public\segredo-das-fadas.jpg',
+            imagem:'/segredo-das-fadas.jpg',
         }
     ]
 
     //O parâmetro "e" da minha função será o meu evento que ocorreu
     function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
-        console.log(e.target.value)
+        //console.log(e.target.value)
+        //Como eu faço para mudar o texto para "TERE"
+        setTexto(e.target.value)
     }
+
     return (
         <>
             <div className="campo-de-pesquisa">
@@ -45,20 +50,17 @@ export default function Main() {
                        {texto && <p>Resultados para: {texto} </p>}
             </div>
             <main className="content-main">
-                <Filme titulo='Barbie'
-                sinopse= "Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade."
-                imagem='/barbie.png'
-                />
-
-                <Filme titulo='Barbie: Fairytopia'
-                sinopse='Logo após o arco-íris, em um mundo chamado Fairytopia, vive Elina, uma bela fada das flores que sonha em ter asas. Um dia, Elina percebe que sua casa de flor está doente e seus amigos não podem mais voar. Cheia de coragem, ela parte em uma fantástica jornada para encontrar Azura, a guardiã da Cidade das Fadas, já que ela não é afetada pelo feitiço que causou todos os problemas.'
-                imagem='public/fairytopia.jpg'
-                />
-
-                <Filme titulo='Barbie: Segredo das Fadas'
-                sinopse= "Mundos mágicos, fadas e cavalos voadores são alguns dos elementos encontrados neste filme da Barbie. Ela precisa viajar a um mundo encantado para salvar Ken. Nesta aventura, serão revelados segredos e algumas amizades serão testadas."
-                imagem='public\segredo-das-fadas.jpg'
-                />
+                {   
+                    filmes.filter((filme)=>filme.titulo.toLowerCase().includes(texto)).map(
+                        (filme)=>
+                            <Filme 
+                                key={filme.id}
+                                sinopse={filme.sinopse}
+                                titulo={filme.titulo}
+                                imagem={filme.imagem}
+                            />
+                    )
+                }
             </main>
         </>
     )
